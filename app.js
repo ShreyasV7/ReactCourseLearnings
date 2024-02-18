9004bd0ad9bd3626182ef8b5eda4f93cb3a7daaa
Shreyas Vishwakarma <coding7id@gmail.com>
2024-02-18 23:32:09 +0530

    2nd chapter

--- app.js
+++ app.js
@@ -7,12 +7,14 @@
    </div>
 */
 
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 
 
 const heading = React.createElement("div",{id:'parent'},
    React.createElement("div",{id:'child'},
      React.createElement("h1",{},"I'm Shreyas Vishwakarma"),
-     React.createElement("h2",{},"I'm Google Chrome")
+      React.createElement("h2", {}, "I'm Brave Browser")
    )
 ) ;  
 const root = ReactDOM.createRoot(document.getElementById("root")) ; 

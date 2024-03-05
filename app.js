5ceb8e5fc9ee688808ccd89a2ff854ca52d28fee
Shreyas Vishwakarma <coding7id@gmail.com>
2024-03-06 01:25:23 +0530

    3rd Chapter

--- app.js
+++ app.js
@@ -2,18 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-// React Element
-
-const heading = React.createElement("h1", { id: "heading" }, "Namaste Shreyas Vishwakarma");
-
-console.log(heading) ; 
-
 // JSX
 
-const jsxHeading = <h1>This is Shreyas form JSX</h1>;
-console.log(jsxHeading);
+const Component2 = () => {
+       return <>
+
+              <h3>This is Component 2</h3>
+       </>
+}
+
+const Component1 = () => {
+       return <>
+
+              <h3>This is Component 1</h3>
+       </>
+}
+//console.log(jsxHeading);
+
+const HeadingComponent = () => {
+       return <>
+              <Component1 />
+              <Component2 />
+              <h1>{488 + 900}</h1>
+              <h2>{console.log("dfsbhnsmn")}</h2>
+              <h2 className="heading">This is Shreyas from Component</h2>
+       </>
+}
+
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
 
 

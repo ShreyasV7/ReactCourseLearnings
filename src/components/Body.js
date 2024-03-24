dc0d246b04a864aa8b4172dd3851de0af7027b9e
Shreyas Vishwakarma <coding7id@gmail.com>
2024-03-25 03:11:09 +0530

    End of Chap 5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockdata";
+import { useState } from "react";
+import resList from "../utils/mockdata";
 
 
 const Body = () => {
+
+       const [bestRes, setBestRes] = useState(resList)
+
+
        return (
               <div className="body">
-                     <div className="search">Search</div>
+                     <div className="filter">
+                            <button className="filter-btn"
+                                   onClick={() => {
+                                          let filteredList = bestRes.filter((res) =>
+                                                 res.info.avgRating > 4.5
+                                          )
+
+                                          setBestRes(filteredList);
+                                          //console.log(bestRes)
+                                   }}
+
+                            > Top Rated Restaurants</button>
+                     </div>
                      <div className="res-container">
-                            {resList.map((restaurant, index) => (
+                            {bestRes.map((restaurant, index) => (
                                    <RestaurantCard key={index} resObj={restaurant} />
                             ))}
                      </div>

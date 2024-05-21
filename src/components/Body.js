410e269820ff7e13255069840a71cc88a06c2050
Shreyas Vishwakarma <coding7id@gmail.com>
2024-05-22 01:35:05 +0530

    Last commit on Chapter-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,85 +5,94 @@ import resList from "../utils/mockdata";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-	const [bestRes, setBestRes] = useState([]);
-	const [filterRes,setFilterRes]=useState([]); 
-	const [searchText, setSearchText] = useState("");
+    const [bestRes, setBestRes] = useState([]);
+    const [filterRes, setFilterRes] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-	let fetchData = async () => {
-		console.log("inside useeffect");
+    let fetchData = async () => {
+        console.log("inside useeffect");
 
-		let data = await fetch(
-			"https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-		);
+        let data = await fetch(
+            "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+            {
+                headers: {
+                    "x-cors-api-key": "temp_1c367bd33eaeac1a419504051f7f38c3",
+                },
+            }
+        );
 
-		let jsonData = await data.json();
-		console.log(jsonData);
-		setBestRes(
-			jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-		);
+        let jsonData = await data.json();
+        console.log(jsonData);
+        setBestRes(
+            jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+                ?.restaurants
+        );
 
-		setFilterRes(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-		); 
-	};
+        setFilterRes(
+            jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+                ?.restaurants
+        );
+    };
 
-	// Conditional Rendering
+    // Conditional Rendering
 
-	console.log("Body Rendered");
-	//console.log(bestRes);
+    console.log("Body Rendered");
+    //console.log(bestRes);
 
-	return bestRes.length == 0 ? (
-		<Shimmer />
-	) : (
-		<div className="body">
-			<div className="filter">
-				<div className="search">
-					<input
-						type="text"
-						className="search-box"
-						value={searchText}
-						onChange={(e) => {
-							setSearchText(e.target.value);
-						}}
-					/>
-					<button
-						onClick={() => {
-							console.log(searchText)
-							let filteredRes = bestRes.filter(
-								(r) => r.info.name.toLowerCase().includes(searchText.toLowerCase())
+    return bestRes.length == 0 ? (
+        <Shimmer />
+    ) : (
+        <div className="body">
+            <div className="filter">
+                <div className="search">
+                    <input
+                        type="text"
+                        className="search-box"
+                        value={searchText}
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                        }}
+                    />
+                    <button
+                        onClick={() => {
+                            console.log(searchText);
+                            let filteredRes = bestRes.filter((r) =>
+                                r.info.name
+                                    .toLowerCase()
+                                    .includes(searchText.toLowerCase())
+                            );
 
-							);
-							
-							setFilterRes(filteredRes);
-						}}
-					>
-						Search
-					</button>
-				</div>
+                            setFilterRes(filteredRes);
+                        }}
+                    >
+                        Search
+                    </button>
+                </div>
 
-				<button
-					className="filter-btn"
-					onClick={() => {
-						let filteredList = bestRes.filter(
-							(res) => res.info.avgRating > 4
-						);
-						setFilterRes(filteredList);
-						//console.log(bestRes)
-					}}
-				>
-					Top Rated Restaurants
-				</button>
-			</div>
-			<div className="res-container">
-				{filterRes.map((restaurant, index) => (
-					<RestaurantCard key={index} resObj={restaurant} />
-				))}
-			</div>
-		</div>
-	);
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        let filteredList = bestRes.filter(
+                            (res) => res.info.avgRating > 4
+                        );
+                        setFilterRes(filteredList);
+                        //console.log(bestRes)
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
+            </div>
+            <div className="res-container">
+                {filterRes.map((restaurant, index) => (
+                    <RestaurantCard key={index} resObj={restaurant} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,42 @@
 import { useState } from "react";
-import { LOGO_URL } from "../utils/constants";
+import LOGO_URL from "../utils/constants";
 
 const Header = () => {
+    //  let btnInOut = 'Login';
 
-       //  let btnInOut = 'Login'; 
+    let [btnLogin, setBtnLogin] = useState("Login");
 
-       let [btnLogin, setBtnLogin] = useState("Login");
+    console.log("Header Rendered");
 
-       console.log('Header Rendered');
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src="https://cdn-icons-png.flaticon.com/512/6122/6122447.png"
+                />
+            </div>
 
-       return (
-              <div className="header">
-                     <div className="logo-container">
-                            <img
-                                   className="logo"
-                                   src={LOGO_URL}
-                            />
-                     </div>
-
-                     <div className="nav-items">
-                            <ul>
-                                   <li>Home</li>
-                                   <li>About</li>
-                                   <li>Contact Us</li>
-                                   <li>Cart</li>
-                                   <button className="login" onClick={() => {
-                                          btnLogin == 'Login' ? setBtnLogin('Logout') : setBtnLogin('Login')
-                                   }}>{btnLogin}</button>
-                            </ul>
-                     </div>
-              </div>
-       );
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button
+                        className="login"
+                        onClick={() => {
+                            btnLogin == "Login"
+                                ? setBtnLogin("Logout")
+                                : setBtnLogin("Login");
+                        }}
+                    >
+                        {btnLogin}
+                    </button>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
-export default Header; 
+export default Header;
